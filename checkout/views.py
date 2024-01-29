05c4d81e5fa4e72a5c4261a04965ff2e37cf98fa
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-01-29 21:30:00 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Oe1qBL7H8GMiIX4bVsKyZRwdghG9byXaSF7JubTnFxqRoJCN38hAiCfW8mxTJLg0V3ivEd1iaV6CbeRVmssIBQ800uww5vMOb',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
