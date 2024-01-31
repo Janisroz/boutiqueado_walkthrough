fd742d6b8731f4cfbc96a87b38799f25e97c6297
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-01-31 21:37:48 +0000

    Added webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,4 +5,14 @@ class StripeWH_Handler:
     Handle Stripe webhooks
     """
 
-    def __innit__(self, request)
+    def __innit__(self, request):
+        self.request = request
+
+    def handle_event(self,event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content= f'Webhook received: {event["type"]}',
+            status=200
+        )
