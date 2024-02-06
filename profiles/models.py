bbacaa370fcf1da51bba8233749910ae7ff2e9e3
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-02-06 21:36:47 +0000

    Updated all auth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -27,11 +27,11 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instnace, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
     if created:
-        UserProfile.objects.create(user=instnace)
+        UserProfile.objects.create(user=instance)
     # Existing Users: Just save the profile
-    instnace.userprofile.save()
+    instance.userprofile.save()
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 def profile(request):
     """ Display the Users Profile """
-    template= 'profiles.html/profile.html'
-    context = {}
+    profile = get_object_or_404(UserProfile, user=request.user)
+    template= 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
