59b7dd81b59f335f74ad7e3d19589b9571e7cdf9
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-02-10 15:09:25 +0000

    Fixed image field

--- products/forms.py
+++ products/forms.py
@@ -1,14 +1,15 @@
 from django import forms
-from .models import Product, Category
 from .widgets import CustomClearableFileInput
+from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
+
     class Meta:
         model = Product
         fields = '__all__'
-    
-    image = forms.ImageField(label='Image', required = False, widget = CustomClearableFileInput)
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,5 +17,5 @@ class ProductForm(forms.ModelForm):
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-        for friendly_name, field in self.fields.items():
+        for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
