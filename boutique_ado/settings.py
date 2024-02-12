2114fe4f7293c7dec815f19b9a6dbf68dd7edbc1
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-02-12 22:02:39 +0000

    s3 push

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -179,23 +179,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-
-    # Cache control
+    # cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
+            'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+            'CacheControl': 'max-age=94608000',
+        }
 
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'boutiqueadoj2'
